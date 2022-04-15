8c1be8f185bd9a2b39f1bb0319e80e7e819593dc
edisionnano <samantas5855@gmail.com>
2022-04-15 21:44:38 +0300

    Now we can make a flashble zip

--- mesa.sh
+++ mesa.sh
@@ -1,8 +1,12 @@
+clear
+soname=$(ls /system/vendor/lib64/hw/|grep vulkan)
 cd /data/data/com.termux/files/home
 rm -rf mesa
 rm -rf .local/share/meson/native/mesa
-mkdir -p .local/share/meson/native
-mkdir mesa
+mkdir -p mesa/magisk/META-INF/com/google/android
+mkdir -p mesa/magisk/system/vendor/lib64/hw
+curl -s https://raw.githubusercontent.com/edisionnano/Freedreno_Turnip_Termux/main/update-binary > mesa/magisk/META-INF/com/google/android/update-binary
+echo "#MAGISK" > mesa/magisk/META-INF/com/google/android/updater-script
 cd mesa
 mkdir files
 yes|pkg update
@@ -17,7 +21,18 @@ rm libdrm.tar.xz
 sed -i 's#prefix=#&/data/data/com.termux/files/home/mesa/files#g' files/usr/lib/pkgconfig/libdrm.pc
 git clone --depth 1 --branch main https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
+version=$(cat VERSION)
+echo "id=turnip\nname=Freedreno Turnip\nversion=$version\nversionCode=1\nauthor=Samantas5855\ndescription=Open Source Vulkan driver for Adreno GPUs, part of the MESA project" > ../magisk/module.prop
 wget https://raw.githubusercontent.com/edisionnano/Freedreno_Turnip_Termux/main/timespec_get.diff
 git apply timespec_get.diff
 meson build-android-aarch64 --native-file mesa -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=32 -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dfreedreno-kgsl=true -Dcpp_rtti=false -Db_lto=true
 ninja -C build-android-aarch64
+patchelf --set-soname $soname build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so
+mv build-android-aarch64/src/freedreno/vulkan/libvulkan_freedreno.so ../magisk/system/vendor/lib64/hw/$soname
+cd ../magisk
+zip -r -q turnip.zip *
+mv turnip.zip /storage/emulated/0
+cd /data/data/com.termux/files/home
+rm -rf mesa
+clear
+printf "Compilation finished, flash the file turnip.zip located at your home folder using Magisk Manager.\n"
